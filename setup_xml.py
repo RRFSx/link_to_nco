b0d1a76ccba4d247d0332a6acbcd4d5f676488a4
guoqing.ge <Guoqing.Ge@noaa.gov>
2024-07-22 22:29:30 -0600

    link rap to the nco namespace

---
+++ setup_xml.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+#
+import os, sys, stat
+from xml_funcs.base import header_begin, header_entities, header_end, source, \
+  wflow_begin, wflow_log, wflow_cycledefs, wflow_end
+from xml_funcs.tasks1 import rap
+
+### setup_xml
+def setup_xml(expdir):
+  # source the config cascade
+  source('exp.setup')
+  #
+  dcCycledef={}
+  dcCycledef['rap']=os.getenv('CYCLEDEF_RAP')
+  dcCycledef['rap_g2']=os.getenv('CYCLEDEF_RAP_G2')
+
+  fPath=f"{expdir}/link2.xml"
+  with open(fPath, 'w') as xmlFile:
+    header_begin(xmlFile)
+    header_end(xmlFile)
+    wflow_begin(xmlFile)
+    log_fpath=f'{expdir}/logs/rrfs.@Y@m@d/@H/link.log'
+    wflow_log(xmlFile,log_fpath)
+    wflow_cycledefs(xmlFile,dcCycledef)
+    
+    # assemble tasks for an experiment or setup/generate an xml file
+    rap(xmlFile,expdir)
+    #rap_g2(xmlFile,expdir)
+    #
+    wflow_end(xmlFile)
+
+  fPath=f"{expdir}/run_rocoto.sh"
+  with open(fPath,'w') as rocotoFile:
+    text= \
+f'''#!/usr/bin/env bash
+source /etc/profile
+module load rocoto
+cd {expdir}
+rocotorun -w rrfs.xml -d rrfs.db
+'''
+    rocotoFile.write(text)
+
+  # set run_rocoto.sh to be executable
+  st = os.stat(fPath)
+  os.chmod(fPath, st.st_mode | stat.S_IEXEC)
+
+  print(f'link.xml and run_rocoto.sh has been created.')
+### end of setup_xml
+
+### run setup_xml.py from the command line
+if __name__ == "__main__":
+  expdir = os.path.dirname(os.path.realpath(__file__))
+  setup_xml(expdir)
---
+++ xml_funcs/tasks1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import os
+from xml_funcs.base import xml_task, source, get_cascade_env
+
+### begin of rap --------------------------------------------------------
+def rap(xmlFile, expdir):
+  meta_id='rap'
+  cycledefs='rap'
+  # metatask (nested or not)
+  length=21
+  interval=1
+  meta_hr= ''.join(f'{i:02d} ' for i in range(0,length+1,interval)).strip()
+  command=f'{expdir}/rap.link_to_nco_namespace.sh'
+  meta_bgn=f'''
+<metatask name="{meta_id}">
+<var name="fhr">{meta_hr}</var>'''
+  meta_end=f'\
+</metatask>\n'
+  task_id=f'{meta_id}_f#fhr#'
+
+  COMINrap=os.getenv("COMINrap","COMINrap_not_defined")
+  # Task-specific EnVars beyond the task_common_vars
+  dcTaskEnv={
+    'CDATE': '<cyclestr>@Y@m@d@H</cyclestr>',
+    'GRBFILE': f'<cyclestr>{COMINrap}/@y@j@H0000#fhr#</cyclestr>'
+  }
+
+  # dependencies
+  timedep=""
+  realtime=os.getenv("REALTIME","false")
+  starttime=get_cascade_env(f"STARTTIME_{task_id}".upper())
+  if realtime.upper() == "TRUE":
+    timedep=f'\n    <timedep><cyclestr offset="{starttime}">@Y@m@d@H@M00</cyclestr></timedep>'
+  dependencies=f'''
+  <dependency>
+  <and>{timedep}
+    <datadep age="00:01:00"><cyclestr>{COMINrap}/@y@j@H0000#fhr#</cyclestr></datadep>
+  </and>
+  </dependency>'''
+  #
+  xml_task(xmlFile,expdir,task_id,cycledefs,dcTaskEnv,dependencies,True,meta_id,meta_bgn,meta_end,command)
+### end of rap --------------------------------------------------------
+
